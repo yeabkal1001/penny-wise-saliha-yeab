3328147ef43dc4d16249abe4f8b9dfac9507f7cb
yeabkal1001 <yeabsira.kayel@bitscollege.edu.et>
2026-01-25 11:42:44 +0300

    Add mobile/constants/categories.js

---
+++ mobile/constants/categories.js
@@ -0,0 +1,14 @@
+export const CATEGORIES = [
+  { id: "food", name: "Food & Drinks", icon: "fast-food" },
+  { id: "groceries", name: "Groceries", icon: "basket" },
+  { id: "shopping", name: "Shopping", icon: "cart" },
+  { id: "transportation", name: "Transportation", icon: "car" },
+  { id: "rent", name: "Rent", icon: "home" },
+  { id: "entertainment", name: "Entertainment", icon: "film" },
+  { id: "medicine", name: "Medicine", icon: "medkit" },
+  { id: "gifts", name: "Gifts", icon: "gift" },
+  { id: "savings", name: "Savings", icon: "wallet" },
+  { id: "bills", name: "Bills", icon: "receipt" },
+  { id: "income", name: "Income", icon: "cash" },
+  { id: "other", name: "Other", icon: "ellipsis-horizontal" },
+];
